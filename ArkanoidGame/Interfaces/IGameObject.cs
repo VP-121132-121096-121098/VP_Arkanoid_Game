477daaf22f323d957f43cc5e6e27b39edb3ceecc
Petar Kimov <petar.kimov@gmail.com>
2014-04-18 22:46:08 +0200

    Update IGameObject

--- ArkanoidGame/Interfaces/IGameObject.cs
+++ ArkanoidGame/Interfaces/IGameObject.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 
@@ -7,5 +8,11 @@ namespace ArkanoidGame.Interfaces
 {
     public interface IGameObject
     {
+        void OnUpdate();
+
+        void OnDraw(Graphics graphics, int frameWidth, int frameHeight);
+
+        public int PositionX {get; set;}
+        public int PositionY { get; set; }
     }
 }
