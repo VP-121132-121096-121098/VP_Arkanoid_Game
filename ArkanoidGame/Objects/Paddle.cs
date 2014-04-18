5b7abf9e29bc773203e8885717ddaead09914ae2
Petar Kimov <petar.kimov@gmail.com>
2014-04-18 22:46:23 +0200

    Created Paddle

--- ArkanoidGame/Objects/Paddle.cs
+++ ArkanoidGame/Objects/Paddle.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using ArkanoidGame.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace ArkanoidGame.Objects
 {
-    class Paddle
+    public class Paddle : IGameObject
     {
+        private float speedX, speedY;
+        
     }
 }
