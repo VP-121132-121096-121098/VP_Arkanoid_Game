38ff8a51520f9d2091b124fd6d150061efb382db
Petar Kimov <petar.kimov@gmail.com>
2014-04-28 08:42:57 +0200

    segsgesgs

---
+++ ArkanoidGame/ArkanoidGamePlayState.cs
@@ -0,0 +1,179 @@
+﻿using ArkanoidGame.Framework;
+using ArkanoidGame.Geometry;
+using ArkanoidGame.Interfaces;
+using ArkanoidGame.Objects;
+using ArkanoidGame.Renderer;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ArkanoidGame
+{
+    public class ArkanoidGamePlayState : IGameState
+    {
+        private bool debugMode;
+
+        public ArkanoidGamePlayState(IGame game)
+        {
+            debugMode = false;
+            this.Game = game;
+            BitmapsToRender = new List<IList<GameBitmap>>();
+            rendererBitmaps = new List<IList<GameBitmap>>();
+            List<GameBitmap> background = new List<GameBitmap>();
+            background.Add(new GameBitmap("\\Resources\\Images\\background.jpg", 0, 0, game.VirtualGameWidth,
+                game.VirtualGameHeight));
+            BitmapsToRender.Add(background);
+
+            //додади го играчот на позиција (1750, 2010).
+            PlayerPaddle player = new PlayerPaddle(new Vector2D(1750, 2010), Game.VirtualGameWidth,
+                Game.VirtualGameHeight);
+            Game.GameObjects.Add(player);
+
+            ElapsedTime = 0;
+        }
+
+        public void OnDraw(Graphics graphics, int frameWidth, int frameHeight)
+        {
+            Game.Renderer.Render(rendererBitmaps, graphics, frameWidth, frameHeight);
+        }
+
+        public long ElapsedTime { get; private set; }
+
+        public int OnUpdate(IList<IGameObject> gameObjects)
+        {
+            EnableOrDisableDebugMode();
+
+            if (Game.IsMultithreadingEnabled)
+            {
+                UpdateObjectsMT(gameObjects);
+            }
+            else
+            {
+                UpdateObjectsST(gameObjects);
+            }
+
+            //Спреми ги текстурите
+            for (int i = 0; i < gameObjects.Count; i++)
+            {
+                if (i + 1 >= BitmapsToRender.Count)
+                    BitmapsToRender.Add(gameObjects[i].ObjectTextures);
+                else
+                    BitmapsToRender[i + 1] = gameObjects[i].ObjectTextures;
+            }
+
+            //Посебна readonly копија за рендерерот
+            CreateCopyForRenderer();
+
+            ElapsedTime++; //поминал еден период
+
+            if (KeyStateInfo.GetAsyncKeyState(Keys.Escape).IsPressed)
+            {
+                return 0;
+            }
+
+            return 100;
+        }
+
+        private void EnableOrDisableDebugMode()
+        {
+            if (KeyStateInfo.GetAsyncKeyState(Keys.D).IsPressed)
+            {
+                debugMode = !debugMode;
+            }
+        }
+
+        /// <summary>
+        /// Повикува Update на сите објекти од еден единствен thread.
+        /// </summary>
+        /// <param name="gameObjects"></param>
+        private void UpdateObjectsST(IList<IGameObject> gameObjects)
+        {
+            foreach (IGameObject obj in gameObjects)
+                obj.OnUpdate(ElapsedTime);
+        }
+
+        private static ManualResetEvent resetEvent = new ManualResetEvent(false);
+
+        /// <summary>
+        /// Повикува Update на секој објект во посебен thread. Се користи ThreadPool.
+        /// </summary>
+        /// <param name="gameObjects"></param>
+        private void UpdateObjectsMT(IList<IGameObject> gameObjects)
+        {
+            //CountdownEvent
+            /* http://msdn.microsoft.com/en-us/library/dd997365.aspx */
+
+            using (CountdownEvent e = new CountdownEvent(1))
+            {
+                // fork work: 
+                foreach (IGameObject obj in gameObjects)
+                {
+                    // Dynamically increment signal count.
+                    e.AddCount();
+                    ThreadPool.QueueUserWorkItem(delegate(object gameObject)
+                    {
+                        try
+                        {
+                            UpdateObject((IGameObject)gameObject);
+                        }
+                        finally
+                        {
+                            e.Signal();
+                        }
+                    },
+                     obj);
+                }
+                e.Signal();
+
+                // The first element could be run on this thread. 
+
+                // Join with work.
+                e.Wait();
+            }
+        }
+
+        private void UpdateObject(IGameObject obj)
+        {
+            obj.OnUpdate(ElapsedTime);
+        }
+
+        private void CreateCopyForRenderer()
+        {
+            List<IList<GameBitmap>> tempList = new List<IList<GameBitmap>>();
+            for (int i = 0; i < BitmapsToRender.Count; i++)
+            {
+                tempList.Add(new List<GameBitmap>());
+                for (int j = 0; j < BitmapsToRender[i].Count; j++)
+                {
+                    tempList[i].Add(BitmapsToRender[i][j]);
+                }
+            }
+            rendererBitmaps = tempList;
+        }
+
+        public IGame Game { get; private set; }
+
+        public bool IsTimesynchronizationImportant
+        {
+            get { return true; }
+        }
+
+        public IList<IList<GameBitmap>> BitmapsToRender { get; private set; } //текстури за секој објект
+
+        private IList<IList<GameBitmap>> rendererBitmaps; /* Копија од листата BitmapsToRender. Бидејќи
+                                                           * листата BitmapsToRender може да се менува,
+                                                           * а Draw е посебен thread, за да се избегне 
+                                                           * синхронизација помеѓу нишките (што ќе го забави
+                                                           * времето на извршување), може да се прати
+                                                           * копија од листата на слики од последното извршување 
+                                                           * на OnUpdate(). Бидејќи ќе пратиме копија ако дојде 
+                                                           * до отстранување на некоја слика од BitmapsToRender, таа
+                                                           * промена ќе се прикаже на првиот повик на Draw по копирањето 
+                                                           * на сликите во листата rendererBitmaps.
+                                                           */
+    }
+}
---
+++ ArkanoidGame/ArkanoidMainMenuState.cs
@@ -0,0 +1,196 @@
+﻿using ArkanoidGame.Framework;
+using ArkanoidGame.Interfaces;
+using ArkanoidGame.Renderer;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ArkanoidGame
+{
+    public class ArkanoidMainMenuState : IGameState
+    {
+
+        private IDictionary<string, GameBitmap> menuOptions; //опции во менито
+        private IDictionary<string, GameBitmap> readyStrings; //стрингови
+
+        public IList<IList<GameBitmap>> BitmapsToRender { get; private set; } //слики од позадината и сите опции заедно
+        private IList<IList<GameBitmap>> rendererBitmaps; /* на рендерерот се праќа копија од листа
+                                                           * за да се избегнат проблемите со нејзино модифицирање
+                                                           */
+
+        public void OnDraw(Graphics graphics, int frameWidth, int frameHeight)
+        {
+            Game.Renderer.Render(rendererBitmaps, graphics, frameWidth, frameHeight);
+        }
+
+        public int OnUpdate(IList<IGameObject> gameObjects)
+        {
+            Point cursor = Game.CursorIngameCoordinates;
+
+            GameBitmap startGame = menuOptions["start game"];
+
+            if (MenuOptionHover(cursor, startGame))
+            {
+                BitmapsToRender[1][0] = readyStrings["start game hover"];
+                if (KeyStateInfo.GetAsyncKeyState(Keys.LButton).WasPressedAfterPreviousCall
+                        && KeyStateInfo.GetAsyncKeyState(Keys.LButton).IsPressed)
+                {
+                    //Ако се притисне глушецот на quit game тогаш излези нормално
+                    Game.GameState = new ArkanoidGamePlayState(Game);
+
+
+
+                    foreach (KeyValuePair<string, GameBitmap> bitmap in readyStrings)
+                    {
+                        RendererCache.RemoveBitmapFromMainMemory(bitmap.Value.PictureID);
+                    }
+
+                    return 100;
+                }
+            }
+            else
+            {
+                BitmapsToRender[1][0] = readyStrings["start game"];
+            }
+
+            GameBitmap quitGame = menuOptions["quit game"];
+            if (MenuOptionHover(cursor, quitGame))
+            {
+                BitmapsToRender[1][1] = readyStrings["quit game hover"];
+                if (KeyStateInfo.GetAsyncKeyState(Keys.LButton).WasPressedAfterPreviousCall
+                        && KeyStateInfo.GetAsyncKeyState(Keys.LButton).IsPressed)
+                {
+                    //Ако се притисне глушецот на quit game тогаш излези нормално
+                    return 0;
+                }
+            }
+            else
+            {
+                BitmapsToRender[1][1] = readyStrings["quit game"];
+            }
+
+            GameBitmap controls = menuOptions["controls"];
+            if (MenuOptionHover(cursor, controls))
+            {
+
+                if (Game.IsControllerMouse)
+                {
+                    BitmapsToRender[1][2] = readyStrings["Game controls mouse hover"];
+                }
+                else
+                {
+                    BitmapsToRender[1][2] = readyStrings["Game controls keyboard hover"];
+                }
+
+
+                if (KeyStateInfo.GetAsyncKeyState(Keys.LButton).WasPressedAfterPreviousCall
+                        && KeyStateInfo.GetAsyncKeyState(Keys.LButton).IsPressed)
+                {
+                    //Ако се притисне глушецот на quit game тогаш излези нормално
+                    if (BitmapsToRender[1][2] == readyStrings["Game controls mouse hover"])
+                    {
+                        BitmapsToRender[1][2] = readyStrings["Game controls keyboard hover"];
+                        Game.IsControllerMouse = false;
+                    }
+                    else
+                    {
+                        BitmapsToRender[1][2] = readyStrings["Game controls mouse hover"];
+                        Game.IsControllerMouse = true;
+                    }
+                }
+            }
+            else if (Game.IsControllerMouse)
+            {
+                BitmapsToRender[1][2] = readyStrings["Game controls mouse"];
+            }
+            else
+            {
+                BitmapsToRender[1][2] = readyStrings["Game controls keyboard"];
+            }
+
+            //Посебна readonly копија за рендерерот
+            List<IList<GameBitmap>> tempList = new List<IList<GameBitmap>>();
+            for (int i = 0; i < BitmapsToRender.Count; i++)
+            {
+                tempList.Add(new List<GameBitmap>());
+                for (int j = 0; j < BitmapsToRender[i].Count; j++)
+                {
+                    tempList[i].Add(BitmapsToRender[i][j]);
+                }
+            }
+            rendererBitmaps = tempList;
+
+            return 100;
+        }
+
+        private static bool MenuOptionHover(Point cursor, GameBitmap menuOptionBitmap)
+        {
+            return (cursor.X >= menuOptionBitmap.X
+                && cursor.X <= menuOptionBitmap.X + menuOptionBitmap.WidthInGameUnits
+                            && cursor.Y >= menuOptionBitmap.Y
+                            && cursor.Y <= menuOptionBitmap.Y + menuOptionBitmap.HeightInGameUnits);
+        }
+
+        /// <summary>
+        /// Слика која ќе се прикажува како позадина на менито
+        /// </summary>
+        public Bitmap MenuBackground { get; private set; }
+
+        public IGame Game { get; set; }
+
+        public ArkanoidMainMenuState(IGame game)
+        {
+            MenuBackground = null;
+            BitmapsToRender = new List<IList<GameBitmap>>();
+            BitmapsToRender.Add(new List<GameBitmap>());
+            BitmapsToRender[0].Add(new GameBitmap("\\Resources\\Images\\background.jpg", 0, 0, game.VirtualGameWidth,
+                game.VirtualGameHeight));
+            rendererBitmaps = new List<IList<GameBitmap>>();
+
+            // додади ги сите опции во меморија
+            readyStrings = new Dictionary<string, GameBitmap>();
+            readyStrings.Add("start game", new GameBitmap(StaticStringFactory.CreateOrangeString("start game"),
+                (game.VirtualGameWidth - 550) / 2, 750, 600, 90));
+            readyStrings.Add("quit game", new GameBitmap(StaticStringFactory.CreateOrangeString("quit game"),
+                (game.VirtualGameWidth - 500) / 2, 1100, 550, 90));
+            readyStrings.Add("start game hover", new GameBitmap(StaticStringFactory.CreateBlueString("start game"),
+                (game.VirtualGameWidth - 550) / 2, 750, 600, 90));
+            readyStrings.Add("quit game hover", new GameBitmap(StaticStringFactory.CreateBlueString("quit game"),
+                (game.VirtualGameWidth - 500) / 2, 1100, 550, 90));
+            readyStrings.Add("Game controls mouse", new GameBitmap(StaticStringFactory
+                .CreateOrangeString("Controls: mouse"), (game.VirtualGameWidth - 750) / 2.0, 920,
+                750, 90));
+            readyStrings.Add("Game controls mouse hover", new GameBitmap(StaticStringFactory
+                .CreateBlueString("Controls: mouse"), (game.VirtualGameWidth - 750) / 2.0, 920,
+                750, 90));
+
+            readyStrings.Add("Game controls keyboard", new GameBitmap(StaticStringFactory
+                .CreateOrangeString("Controls: keyboard"), (game.VirtualGameWidth - 750) / 2.0, 920,
+                750, 90));
+            readyStrings.Add("Game controls keyboard hover", new GameBitmap(StaticStringFactory
+                .CreateBlueString("Controls: keyboard"), (game.VirtualGameWidth - 750) / 2.0, 920,
+                750, 90));
+
+            menuOptions = new Dictionary<string, GameBitmap>();
+            menuOptions.Add("start game", readyStrings["start game"]);
+            menuOptions.Add("quit game", readyStrings["quit game"]);
+            menuOptions.Add("controls", readyStrings["Game controls mouse"]);
+
+            BitmapsToRender.Add(new List<GameBitmap>());
+            BitmapsToRender[1].Add(menuOptions["start game"]);
+            BitmapsToRender[1].Add(menuOptions["quit game"]);
+            BitmapsToRender[1].Add(menuOptions["controls"]);
+
+            this.Game = game;
+            Game.IsControllerMouse = true;
+        }
+
+        public bool IsTimesynchronizationImportant
+        {
+            get { return false; }
+        }
+    }
+}
