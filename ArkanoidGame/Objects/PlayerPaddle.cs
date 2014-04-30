97b673c2c27b932c691da8b768517b174b0e7506
Petar Kimov <petar.kimov@gmail.com>
2014-04-30 16:32:15 +0200

    //

--- ArkanoidGame/Objects/PlayerPaddle.cs
+++ ArkanoidGame/Objects/PlayerPaddle.cs
@@ -78,7 +78,7 @@ namespace ArkanoidGame.Objects
             
             /*GameRectangle proba = new GameRectangle(ObjectTextures[0].PositionUL, ObjectTextures[0].PositionUR,
                 ObjectTextures[0].PositionDL);
-            proba.RotateAroundPointDeg(new Vector2D((1750.0 * 2 + ObjectWidth) / 2, (2010.0 * 2 + ObjectHeight) / 2), 10);
+            //proba.RotateAroundPointDeg(new Vector2D((1750.0 * 2 + ObjectWidth) / 2, (2010.0 * 2 + ObjectHeight) / 2), 10);
             proba.RotateAroundPointDeg(new Vector2D(1750, 1005), 1);
             ObjectTextures[0].PositionUL = proba.PositionUL;
             ObjectTextures[0].PositionUR = proba.PositionUR;
